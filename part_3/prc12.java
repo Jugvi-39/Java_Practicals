679201250c69507d442b4ca3ff7da165c91cd605
Jugvi-39 <23dcs021@charusat.edu.in>
2024-07-30 09:57:20 +0530

    Create prc12.java

---
+++ part_3/prc12.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class prc12
+{
+	public static void main(String a[])
+	{
+		//System.out.println("Enter the value in pounds: ");
+		int x=Integer.parseInt(a[0]);
+		int y=x*100;
+		System.out.println("The value is rupees is: "+y);
+	}
+}
