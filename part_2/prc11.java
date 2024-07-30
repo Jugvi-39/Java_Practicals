dd4f0263a68cfdf5202c308caeb6a244b1bd4acc
Jugvi-39 <23dcs021@charusat.edu.in>
2024-07-30 09:52:15 +0530

    Create prc11.java

---
+++ part_2/prc11.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class prc11
+{
+	public static void main(String a[])
+	{
+		String str="CHARUSAT UNIVERSITY";
+		System.out.println("Given string: "+str);
+		
+		System.out.println("The length is: "+str.length());
+		
+		System.out.println("The string in lower case is: "+str.toLowerCase());
+		
+		System.out.println("The modified string is: "+str.replace('H','J'));
+		
+		
+	}
+}
