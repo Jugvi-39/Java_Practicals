b9f4630ab9187d2ac328ef920fac1a8455dcdd2f
Jugvi-39 <23dcs021@charusat.edu.in>
2024-09-17 21:45:47 +0530

    Create prc25.java

---
+++ part_5/prc25.java
@@ -0,0 +1,18 @@
+public class prc25
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			throwMethod();
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+	static void throwMethod()
+	{
+		throw new ArithmeticException("User Thrown Exception");
+	}
+}
